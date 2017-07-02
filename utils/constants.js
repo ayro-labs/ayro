dca715153e6003513cdf547f32ebb3dc42142a68
Sandro Simas <sandro.csimas@gmail.com>
2017-07-02 02:13:06 -0300

    Improving constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,22 +1,22 @@
 'use strict';
 
-exports.integration = {
-  channels: {
+exports.integration = Object.freeze({
+  channels: Object.freeze({
     USER: 'user',
     BUSINESS: 'business'
-  },
-  types: {
+  }),
+  types: Object.freeze({
     WEBSITE: 'website',
     ANDROID: 'android',
     IOS: 'ios',
     SLACK: 'slack',
-  }
-};
+  })
+});
 
-exports.device = {
-  platforms: {
+exports.device = Object.freeze({
+  platforms: Object.freeze({
     WEB: 'web',
     ANDROID: 'android',
     IOS: 'ios'
-  }
-};
+  })
+});
