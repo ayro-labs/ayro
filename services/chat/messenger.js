888f12cff45f5681b62c958cef60d9f4c91e8d5e
Sandro Simas <sandro.csimas@gmail.com>
2017-10-08 12:49:24 -0300

    Fixing postMessage from message integration

--- services/chat/messenger.js
+++ services/chat/messenger.js
@@ -8,31 +8,36 @@ const integrationCommons = require('../commons/integration');
 const chatService = require('.');
 
 exports.postMessage = (data) => {
-  return integrationCommons.findIntegration({channel: constants.integration.channels.MESSENGER, 'configuration.page.id': data.recipient.id}, {require: false}).then((integration) => {
-    if (!integration) {
+  return Promise.resolve().then(() => {
+    if (!data.message.text) {
       return null;
     }
-    return deviceCommons.findDevices({'info.profile_id': data.sender.id}, {populate: 'user'}).then((devices) => {
-      const device = devices.find((device) => {
-        return device.user.app.toString() === integration.app.toString();
-      });
-      if (!device) {
-        const deviceData = {
-          uid: hash.uuid(),
-          platform: constants.device.platforms.MESSENGER.id,
-          info: {
-            profile_id: data.sender.id,
-          },
-        };
-        return userCommons.createUser(new App({id: integration.app}), {uid: hash.uuid(), identified: false}).then((user) => {
-          return deviceCommons.createDevice(user, deviceData);
-        }).then((device) => {
-          return Device.populate(device, 'user');
-        });
+    return integrationCommons.findIntegration({channel: constants.integration.channels.MESSENGER, 'configuration.page.id': data.recipient.id}, {require: false}).then((integration) => {
+      if (!integration) {
+        return null;
       }
-      return device;
-    }).then((device) => {
-      return chatService.postMessage(device.user, device, {text: data.message.text});
+      return deviceCommons.findDevices({'info.profile_id': data.sender.id}, {populate: 'user'}).then((devices) => {
+        const device = devices.find((device) => {
+          return device.user.app.toString() === integration.app.toString();
+        });
+        if (!device) {
+          const deviceData = {
+            uid: hash.uuid(),
+            platform: constants.device.platforms.MESSENGER.id,
+            info: {
+              profile_id: data.sender.id,
+            },
+          };
+          return userCommons.createUser(new App({id: integration.app}), {uid: hash.uuid(), identified: false}).then((user) => {
+            return deviceCommons.createDevice(user, deviceData);
+          }).then((device) => {
+            return Device.populate(device, 'user');
+          });
+        }
+        return device;
+      }).then((device) => {
+        return chatService.postMessage(device.user, device, {text: data.message.text});
+      });
     });
   });
 };
