8cb9b912f80abc1464b1b833d2265ae5e490bed3
Sandro Simas <sandro.csimas@gmail.com>
2017-11-22 02:02:28 -0300

    Fixing slack agent image size;

--- services/integrations/slack.js
+++ services/integrations/slack.js
@@ -343,7 +343,7 @@ exports.extractAgent = (configuration, data) => {
     return {
       id: data.user_id,
       name: result.user.profile.real_name,
-      photo_url: result.user.profile.image_72,
+      photo_url: result.user.profile.image_192,
     };
   })();
 };
