7c3b62fd5cb9a54288e639ab85719458264fafc7
Sandro Simas <sandro.csimas@gmail.com>
2017-12-01 02:15:47 -0300

    Improving publish task

--- tasks/publish.js
+++ tasks/publish.js
@@ -3,11 +3,7 @@ const path = require('path');
 const childProcess = require('child_process');
 const Promise = require('bluebird');
 
-const REPOSITORY_URL = 'docker.ayro.io';
-const REPOSITORY_OWNER = 'ayro';
-const REPOSITORY_NAME = `${REPOSITORY_OWNER}/${projectPackage.name}`;
-const REGISTRY_USERNAME = process.env.DOCKER_REGISTRY_USERNAME;
-const REGISTRY_PASSWORD = process.env.DOCKER_REGISTRY_PASSWORD;
+const REPOSITORY_URL = '554511234717.dkr.ecr.us-west-1.amazonaws.com';
 const WORKING_DIR = path.resolve(__dirname, '../');
 
 const execAsync = Promise.promisify(childProcess.exec);
@@ -26,17 +22,16 @@ function checkoutTag(version) {
 function buildImage() {
   return Promise.coroutine(function* () {
     console.log('Building image...');
-    yield exec(`docker build -t ${REPOSITORY_NAME} .`);
+    yield exec(`docker build -t ${projectPackage.name} .`);
     console.log('Tagging image...');
-    yield exec(`docker tag ${REPOSITORY_NAME} ${REPOSITORY_URL}/${REPOSITORY_NAME}`);
+    yield exec(`docker tag ${projectPackage.name}:latest ${REPOSITORY_URL}/${projectPackage.name}:latest`);
   })();
 }
 
 function publishToRegistry() {
   return Promise.coroutine(function* () {
     console.log('Publishing to Registry...');
-    yield exec(`docker login ${REPOSITORY_URL} -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD}`);
-    yield exec(`docker push ${REPOSITORY_URL}/${REPOSITORY_NAME}`);
+    yield exec(`docker push ${REPOSITORY_URL}/${projectPackage.name}:latest`);
   })();
 }
 
